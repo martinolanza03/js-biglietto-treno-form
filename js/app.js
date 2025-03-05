60de8333aa7577a59b1f2f4c8463794cb93e099c
martinolanza03 <lanzamartino3@gmail.com>
2025-03-05 15:43:35 +0100

    feat: first milestone

---
+++ js/app.js
@@ -0,0 +1,32 @@
+const elementNumberKm = document.getElementById('numberKm');
+const elementAge = document.getElementById('age');
+const elementSubmit = document.getElementById('sendForm');
+
+console.log(elementNumberKm);
+console.log(elementAge);
+console.log(elementSubmit);
+
+elementSubmit.addEventListener('click', function(event){
+    event.preventDefault();
+
+    let costTicket = parseInt(0);
+    let discount = parseInt(0);
+
+    const age = elementAge.value;
+    const km = elementNumberKm.value;
+
+    if (age >= 18 && age<= 64) {
+        costTicket = km * 0.21;
+    } else if (age <= 17) {
+        costTicket = (km * 0.21);
+        discount = (costTicket * 20) /100;
+        costTicket = costTicket - discount;
+    } else if (age >= 65) {
+        costTicket = km * 0.21;
+        discount = (costTicket * 40) /100;
+        costTicket = costTicket - discount;
+    }
+
+    console.log(parseFloat(costTicket.toFixed(2)));
+});
+
